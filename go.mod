ae820c6dcbb50518bb7f7390aeff30bec62a0fce
Alexander Hermes <alexander.hermes@grasshopperasia.com>
2019-11-22 13:39:40 +0800

    Initial basic implementation based on example
    Simple test implementation of listing users based on the "w" tool

---
+++ go.mod
@@ -0,0 +1,6 @@
+module github.com/ExalDraen/choria-plugin-user-agent
+
+require (
+	github.com/choria-io/go-external v0.0.0-20191122044439-c986598dad73
+	github.com/choria-io/go-external-agent v0.0.0-20190925162635-e84cba4b1f89
+)
---
+++ list_action.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"os/exec"
+
+	"github.com/choria-io/go-external/agent"
+)
+
+// ListAction implements an agent.ActionHandler handler for listing the users
+// that are logged in on a given POSIX system
+func ListAction(request *agent.Request, reply *agent.Reply, config map[string]string) {
+
+	out, err := exec.Command("w --no-header --from").Output()
+	if err != nil {
+		reply.Statuscode = agent.Aborted
+		reply.Statusmsg = fmt.Sprintf("could not list users %s#%s: %s", request.Agent, request.Action, err)
+		return
+	}
+
+	reply.Data = map[string]string{
+		"message": string(out),
+	}
+}
