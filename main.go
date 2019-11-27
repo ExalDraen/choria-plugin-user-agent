4c2f6e99f1331fc26571c5fcd3620fc5982b5f45
Alexander Hermes <alexander.hermes@grasshopperasia.com>
2019-11-27 17:30:54 +0800

    Remove echo action from go code

--- main.go
+++ main.go
@@ -4,29 +4,11 @@ import (
 	"github.com/choria-io/go-external/agent"
 )
 
-type echoRequest struct {
-	Message string `json:"message"`
-}
-
-func echoAction(request *agent.Request, reply *agent.Reply, config map[string]string) {
-	req := &echoRequest{}
-
-	// parse the received request, sets appropriate errors on reply on failure
-	if !request.ParseRequestData(req, reply) {
-		return
-	}
-
-	reply.Data = map[string]string{
-		"message": req.Message,
-	}
-}
-
 func main() {
 	userAgent := agent.NewAgent("user")
 	defer userAgent.ProcessRequest()
 
 	// action will be invoked on demand
-	userAgent.MustRegisterAction("echo", echoAction)
 	userAgent.MustRegisterAction("kill", KillAction)
 	userAgent.MustRegisterAction("list", ListAction)
 }
